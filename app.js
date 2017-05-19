32f856094f41e315c379a93e0fc78a7de251a377
Brent Perry <burentoperii@gmail.com>
2017-05-19 19:30:53 +0000

    add route params

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ app.get("/dog", function(req, res) {
     res.send("Meow");
 });
 
+app.get("/r/:subredditName", function(req, res) {
+    var subreddit = req.params.subredditName;
+    res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+    console.log(req.params);
+    res.send("Welcome to the comments page!");
+});
+
+app.get("*", function(req, res) {
+    res.send("You are a star!!");
+});
+
 // Listen for requests
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("server has started");
